26b0dd573cb09995756720a564a1e801465c72c1
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-12-01 20:04:32 +0100

    Completed day 1 part 2

--- src/01/01.cpp
+++ src/01/01.cpp
@@ -47,5 +47,23 @@ int main() {
         if (stoi(v[i]) > previous) total++;
         previous = stoi(v[i]);
     }
-    std::cout << total << std::endl;
+
+    std::cout << "Part 1: " << total << std::endl;
+
+    previous = 0;
+    auto total2 = 0;
+    for (auto i = 0; i < v.size() - 2; i++) {
+        auto calculated = 0;
+        calculated += stoi(v[i]);
+        calculated += stoi(v[i + 1]);
+        calculated += stoi(v[i + 2]);
+        if (i == 0) {
+            previous = calculated;
+            continue;
+        }
+        if (calculated > previous) total2++;
+        previous = calculated;
+    }
+
+    std::cout << "Part 2: " << total2 << std::endl;
 }
