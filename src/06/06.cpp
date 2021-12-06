b69867caed5bd4cd6b0d32e4302c2e75b8361d2f
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-12-06 13:20:08 +0100

    Completed day 6 (shoutout to my class mates)

--- src/06/06.cpp
+++ src/06/06.cpp
@@ -1,25 +1,67 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
 
 #include "../common/common.h"
 
-int main(){
-	const std::vector<std::string> stringInputs = split(read_inputs(), ",");
-	std::vector<int> inputs;
-	// Stolen from https://stackoverflow.com/a/15619863/9634099
-	std::transform(stringInputs.begin(), stringInputs.end(), std::back_inserter(inputs), [](const std::string& str) { return stoi(str);});
+void substract(std::vector<int>& inputs, int j, int subtract) {
+    inputs[j] -= subtract;
+    if (inputs[j] <= 0) {
+        inputs.push_back(9 + inputs[j] + subtract);
+        inputs[j] += subtract;
+    }
+}
 
-	for (int i = 0; i < 80; ++i) {
-		for (int j = 0; j < inputs.size(); ++j) {
-			if (inputs[j] == 0) {
-				inputs.push_back(9);
-				inputs[j] = 7;
-			}
-			inputs[j]--;
-		}
-	}
+void addToMap(std::map<int, int>& map, int insert) {
+    while (insert > 8) insert -= 8;
+    std::map<int, int>::iterator itr = map.find(insert);
+    if (itr == map.end())
+        map.insert(std::pair<int, int>(insert, 1));
+    else
+        itr->second++;
+}
 
-	std::cout << "Part 1: " << inputs.size() << std::endl;
+std::map<int, int> fillMap(std::vector<int> vector) {
+    std::map<int, int> map;
+    for (int i : vector) addToMap(map, i);
+    return map;
+}
+
+int main() {
+    const std::vector<std::string> stringInputs = split(read_inputs(), ",");
+    std::vector<int> startValues;
+    // Stolen from https://stackoverflow.com/a/15619863/9634099
+    std::transform(stringInputs.begin(), stringInputs.end(),
+                   std::back_inserter(startValues),
+                   [](const std::string& str) { return stoi(str); });
+
+    std::map<int, int> numbers = fillMap(startValues);
+    std::vector<long> inputs{0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i : startValues) inputs[i]++;
+
+    int part1 = 0;
+    for (int i = 0; i < 256; ++i) {
+        long tempOld = 0;
+        long tempNow;
+        for (int j = inputs.size() - 1; j >= 0; --j) {
+            if (j == 0) {
+                inputs[6] += inputs[j];
+                inputs[8] += inputs[j];
+            }
+            tempNow = inputs[j];
+            inputs[j] = tempOld;
+            tempOld = tempNow;
+        }
+
+        if (i == 79)
+            for (long x : inputs) part1 += x;
+    }
+
+    long part2 = 0;
+    for (long x : inputs) part2 += x;
+
+    std::cout << "Part 1: " << part1 << "\nPart 2: " << part2
+              << std::endl;
 }
