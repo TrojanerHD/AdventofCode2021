ff941f5fb92c12f575fee4ce80471ee3bae6630d
TrojanerHD <xxitztrojanerhdpvptvytxx@gmail.com>
2021-12-10 09:03:15 +0100

    Completed day 10

--- src/10/10.cpp
+++ src/10/10.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <regex>
+#include <set>
 #include <stack>
 #include <string>
 #include <vector>
@@ -10,47 +11,70 @@ int main() {
     auto lines = split(read_inputs(), "\n");
     std::stack<char> brackets;
     int points1 = 0;
-		int points2 = 0;
+    std::vector<long> points2{};
     for (auto line : lines) {
+        bool corrupted = false;
         for (auto character : line) {
             std::regex r("[{\\(\\[<]");
             std::string characterString(1, character);
             if (std::regex_match(characterString, r))
                 brackets.push(character);
             else if (character == '}') {
-                if (brackets.top() == '{') {
+                if (brackets.top() == '{')
                     brackets.pop();
-										continue;
-                } else {
+                else {
+                    corrupted = true;
                     points1 += 1197;
-										break;
-								}
+                    break;
+                }
             } else if (character == ')') {
-                if (brackets.top() == '(') {
+                if (brackets.top() == '(')
                     brackets.pop();
-										continue;
-                } else {
+                else {
+                    corrupted = true;
                     points1 += 3;
-										break;
-								}
+                    break;
+                }
             } else if (character == ']') {
-                if (brackets.top() == '[') {
+                if (brackets.top() == '[')
                     brackets.pop();
-										continue;
-                } else {
+                else {
+                    corrupted = true;
                     points1 += 57;
-										break;
-								}
+                    break;
+                }
             } else if (character == '>') {
-                if (brackets.top() == '<') {
+                if (brackets.top() == '<')
                     brackets.pop();
-										continue;
-                } else {
+                else {
+                    corrupted = true;
                     points1 += 25137;
-										break;
-								}
+                    break;
+                }
             }
         }
+        if (corrupted) {
+            brackets = {};
+            continue;
+        }
+        long score = 0;
+        while (!brackets.empty()) {
+            score *= 5;
+            if (brackets.top() == '(') score += 1;
+            if (brackets.top() == '[') score += 2;
+            if (brackets.top() == '{') score += 3;
+            if (brackets.top() == '<') score += 4;
+            brackets.pop();
+            if (score < 0) {
+                score = __LONG_MAX__;
+                while (!brackets.empty()) brackets.pop();
+                break;
+            }
+        }
+        points2.push_back(score);
     }
-    std::cout << "Part 1: " << points1 << std::endl;
+    std::sort(points2.begin(), points2.end());
+    std::vector<long>::iterator it = points2.begin();
+    std::advance(it, points2.size() / 2);
+    std::cout << "Part 1: " << points1 << "\nPart 2: " << *it << std::endl;
 }
